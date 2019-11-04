1b1fc8e418571d6cf6845a79fbf31bc1e7e83b29
nazeh <ar.nazeh@gmail.com>
2019-11-04 19:08:52 +0300

    setup stickler

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
-}
+const App = () => (
+  <div className="App" />
+);
 
 export default App;
