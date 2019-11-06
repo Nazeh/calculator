15727c82a2c44ac163bf59535103e836b517b6d9
nazeh <ar.nazeh@gmail.com>
2019-11-06 15:42:13 +0300

    resolve merge issues

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Display.scss';
 
-const Display = ({ result }) => (
+const Display = ({ total }) => (
   <div className="Display">
-    <p className="Display__result">{result}</p>
+    <p className="Display__total">{total}</p>
   </div>
 );
 
 Display.propTypes = {
-  result: PropTypes.string,
+  total: PropTypes.string,
 };
 
 Display.defaultProps = {
-  result: '0',
+  total: '0',
 };
 
 export default Display;
