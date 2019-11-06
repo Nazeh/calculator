a16cbd8f8e199ec822bc4e927caf3b7e8466f691
nazeh <ar.nazeh@gmail.com>
2019-11-06 20:50:50 +0300

    update event handlers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
+
 import './App.scss';
 
-const App = () => (
-  <div id="App">
-    <Display />
-    <ButtonPanel />
-  </div>
-);
+const App = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: '0',
+    operation: null,
+  });
+
+  const handleClick = (buttonName) => {
+    console.log({ ...state }, buttonName);
+    setState(calculate({ ...state }, buttonName));
+  };
+
+  return (
+    <div id="App">
+      <Display total={state.total || state.next} />
+      <ButtonPanel clickHandler={handleClick} />
+    </div>
+  );
+};
 
 export default App;
--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -1,42 +1,49 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Button from './Button';
 import './ButtonPanel.scss';
 
-const ButtonPanel = () => {
+const ButtonPanel = ({ clickHandler }) => {
   const Grey = '#e0e0e0';
+  const handleClick = (buttonName) => clickHandler(buttonName);
+
   return (
     <div className="ButtonPanel">
       <div className="ButtonPanel__row">
-        <Button name="AC" color={Grey} />
-        <Button name="+/-" color={Grey} />
-        <Button name="%" color={Grey} />
-        <Button name="÷" />
+        <Button name="AC" clickHandler={handleClick} color={Grey} />
+        <Button name="+/-" clickHandler={handleClick} color={Grey} />
+        <Button name="%" clickHandler={handleClick} color={Grey} />
+        <Button name="÷" clickHandler={handleClick} />
       </div>
       <div className="ButtonPanel__row">
-        <Button name="7" color={Grey} />
-        <Button name="8" color={Grey} />
-        <Button name="9" color={Grey} />
-        <Button name="x" />
+        <Button name="7" clickHandler={handleClick} color={Grey} />
+        <Button name="8" clickHandler={handleClick} color={Grey} />
+        <Button name="9" clickHandler={handleClick} color={Grey} />
+        <Button name="x" clickHandler={handleClick} />
       </div>
       <div className="ButtonPanel__row">
-        <Button name="4" color={Grey} />
-        <Button name="5" color={Grey} />
-        <Button name="6" color={Grey} />
-        <Button name="-" />
+        <Button name="4" clickHandler={handleClick} color={Grey} />
+        <Button name="5" clickHandler={handleClick} color={Grey} />
+        <Button name="6" clickHandler={handleClick} color={Grey} />
+        <Button name="-" clickHandler={handleClick} />
       </div>
       <div className="ButtonPanel__row">
-        <Button name="1" color={Grey} />
-        <Button name="2" color={Grey} />
-        <Button name="3" color={Grey} />
-        <Button name="+" />
+        <Button name="1" clickHandler={handleClick} color={Grey} />
+        <Button name="2" clickHandler={handleClick} color={Grey} />
+        <Button name="3" clickHandler={handleClick} color={Grey} />
+        <Button name="+" clickHandler={handleClick} />
       </div>
       <div className="ButtonPanel__row">
-        <Button name="0" color={Grey} wide />
-        <Button name="." color={Grey} />
-        <Button name="=" />
+        <Button name="0" clickHandler={handleClick} color={Grey} wide />
+        <Button name="." clickHandler={handleClick} color={Grey} />
+        <Button name="=" clickHandler={handleClick} />
       </div>
     </div>
   );
+};
+
+ButtonPanel.propTypes = {
+  clickHandler: PropTypes.func.isRequired,
 };
 
 export default ButtonPanel;
